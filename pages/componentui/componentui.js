8e0f4502b1a5039e8059da543900e81f9ae683a9
余彦东 <yuyandong@cloudoc.cn>
2018-08-21 18:10:09 +0800

    update ui test

--- pages/componentui/componentui.js
+++ pages/componentui/componentui.js
@@ -1,5 +1,31 @@
 // pages/componentui/componentui.js
 var order = ['red', 'yellow', 'blue', 'green', 'red']
+const date = new Date()
+const years = []
+const months = []
+const days = []
+
+for (let i = 1990; i <= date.getFullYear() + 100; i++) {
+  years.push(i)
+}
+
+for (let i = 1; i <= 12; i++) {
+  months.push(i)
+}
+
+for (let i = 1; i <= 31; i++) {
+  days.push(i)
+}
+
+function getRandomColor() {
+  let rgb = []
+  for (let i = 0; i < 3; ++i) {
+    let color = Math.floor(Math.random() * 256).toString(16)
+    color = color.length == 1 ? '0' + color : color
+    rgb.push(color)
+  }
+  return '#' + rgb.join('')
+}
 Page({
 
   /**
@@ -80,29 +106,169 @@ Page({
       'success', 'success_no_circle', 'info', 'warn', 'waiting', 'cancel', 'download', 'search', 'clear'
     ],
 
-  /**
-   * show-info在进度条右侧显示百分比
-   */
-    showinfo : true,
+    /**
+     * show-info在进度条右侧显示百分比
+     */
+    showinfo: true,
     //进度条线的宽度，单位px
-    strokewidth : 6,
+    strokewidth: 6,
     color: "#0000ff",
     //已选择的进度条的颜色
     activeColor: "#0000ff",
     //backgroundColor未选择的进度条的颜色
     backgroundColor: "#ccccc",
     //进度条从左往右的动画
-    active : true,
+    active: true,
     //active-mode backwards: 动画从头播；forwards：动画从上次结束点接着播
-    activemode : "forwards",
+    activemode: "forwards",
 
-// button相关设置
+    // button相关设置
     defaultSize: 'default',
     primarySize: 'default',
     warnSize: 'default',
     disabled: false,
     plain: false,
-    loading: false
+    loading: false,
+    array: ['美国', '中国', '巴西', '日本'],
+    objectArray: [{
+        id: 0,
+        name: '美国'
+      },
+      {
+        id: 1,
+        name: '中国'
+      },
+      {
+        id: 2,
+        name: '巴西'
+      },
+      {
+        id: 3,
+        name: '日本'
+      }
+    ],
+    index: 0,
+    multiArray: [
+      ['无脊柱动物', '脊柱动物'],
+      ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'],
+      ['猪肉绦虫', '吸血虫']
+    ],
+    objectMultiArray: [
+      [{
+          id: 0,
+          name: '无脊柱动物'
+        },
+        {
+          id: 1,
+          name: '脊柱动物'
+        }
+      ],
+      [{
+          id: 0,
+          name: '扁性动物'
+        },
+        {
+          id: 1,
+          name: '线形动物'
+        },
+        {
+          id: 2,
+          name: '环节动物'
+        },
+        {
+          id: 3,
+          name: '软体动物'
+        },
+        {
+          id: 3,
+          name: '节肢动物'
+        }
+      ],
+      [{
+          id: 0,
+          name: '猪肉绦虫'
+        },
+        {
+          id: 1,
+          name: '吸血虫'
+        }
+      ]
+    ],
+    multiIndex: [0, 0, 0],
+    date: '2018-08-019',
+    time: '12:01',
+    region: ['广东省', '广州市', '海珠区'],
+    customItem: '全部',
+
+    years: years,
+    year: date.getFullYear(),
+    months: months,
+    month: 2,
+    days: days,
+    day: 2,
+    value: [9999, 1, 1],
+
+    items: [{
+        name: 'USA',
+        value: '美国'
+      },
+      {
+        name: 'CHN',
+        value: '中国',
+        checked: 'true'
+      },
+      {
+        name: 'BRA',
+        value: '巴西'
+      },
+      {
+        name: 'JPN',
+        value: '日本'
+      },
+      {
+        name: 'ENG',
+        value: '英国'
+      },
+      {
+        name: 'TUR',
+        value: '法国'
+      },
+      {
+        name: 'HANGUO',
+        value: '韩国'
+      }
+    ],
+
+    radiocheckedvalue: '中国',
+    height: 20,
+    focus: false,
+    poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    name: '此时此刻',
+    author: '许巍',
+    src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+    inputValue :'',
+    src: '',
+    danmuList: [{
+        text: '第 1s 出现的弹幕',
+        color: '#ff0000',
+        time: 1
+      },
+      {
+        text: '第 3s 出现的弹幕',
+        color: '#ff00ff',
+        time: 3
+      }
+    ],
+    autoplay : false,
+    loop : true,
+    /**
+     * page-gesture在非全屏模式下，是否开启亮度与音量调节手势
+     */
+    pagegesture : false,
+    /**
+     * 当视频大小与 video 容器大小不一致时，视频的表现形式。contain：包含，fill：填充，cover：覆盖
+     */
+    objectFit : 'fill'
 
 
 
@@ -119,7 +285,9 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function() {
-
+    // 使用 wx.createAudioContext 获取 audio 上下文 context
+    this.audioCtx = wx.createAudioContext('myAudio')
+    this.videoContext = wx.createVideoContext('myVideo')
   },
 
   /**
@@ -189,24 +357,196 @@ Page({
     })
   },
 
-  setDisabled: function (e) {
+  setDisabled: function(e) {
     this.setData({
       disabled: !this.data.disabled
     })
   },
-  setPlain: function (e) {
+  setPlain: function(e) {
     this.setData({
       plain: !this.data.plain
     })
   },
-  setLoading: function (e) {
+  setLoading: function(e) {
     this.setData({
       loading: !this.data.loading
     })
   },
-  onGotUserInfo: function (e) {
+  onGotUserInfo: function(e) {
     console.log(e.detail.errMsg)
     console.log(e.detail.userInfo)
     console.log(e.detail.rawData)
   },
+
+  bindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindMultiPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      multiIndex: e.detail.value
+    })
+  },
+  bindMultiPickerColumnChange: function(e) {
+    console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
+    var data = {
+      multiArray: this.data.multiArray,
+      multiIndex: this.data.multiIndex
+    };
+    data.multiIndex[e.detail.column] = e.detail.value;
+    switch (e.detail.column) {
+      case 0:
+        switch (data.multiIndex[0]) {
+          case 0:
+            data.multiArray[1] = ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'];
+            data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
+            break;
+          case 1:
+            data.multiArray[1] = ['鱼', '两栖动物', '爬行动物'];
+            data.multiArray[2] = ['鲫鱼', '带鱼'];
+            break;
+        }
+        data.multiIndex[1] = 0;
+        data.multiIndex[2] = 0;
+        break;
+      case 1:
+        switch (data.multiIndex[0]) {
+          case 0:
+            switch (data.multiIndex[1]) {
+              case 0:
+                data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
+                break;
+              case 1:
+                data.multiArray[2] = ['蛔虫'];
+                break;
+              case 2:
+                data.multiArray[2] = ['蚂蚁', '蚂蟥'];
+                break;
+              case 3:
+                data.multiArray[2] = ['河蚌', '蜗牛', '蛞蝓'];
+                break;
+              case 4:
+                data.multiArray[2] = ['昆虫', '甲壳动物', '蛛形动物', '多足动物'];
+                break;
+            }
+            break;
+          case 1:
+            switch (data.multiIndex[1]) {
+              case 0:
+                data.multiArray[2] = ['鲫鱼', '带鱼'];
+                break;
+              case 1:
+                data.multiArray[2] = ['青蛙', '娃娃鱼'];
+                break;
+              case 2:
+                data.multiArray[2] = ['蜥蜴', '龟', '壁虎'];
+                break;
+            }
+            break;
+        }
+        data.multiIndex[2] = 0;
+        console.log(data.multiIndex);
+        break;
+    }
+    this.setData(data);
+  },
+  bindDateChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindTimeChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      time: e.detail.value
+    })
+  },
+  bindRegionChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      region: e.detail.value
+    })
+  },
+
+  bindChange: function(e) {
+    const val = e.detail.value
+    this.setData({
+      year: this.data.years[val[0]],
+      month: this.data.months[val[1]],
+      day: this.data.days[val[2]]
+    })
+  },
+  radioChange: function(e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value)
+    console.log('radio发生change事件，改变后的值是：', e)
+    this.setData({
+      radiocheckedvalue: e.detail.value
+    })
+  },
+
+  bindButtonTap: function() {
+    this.setData({
+      focus: true
+    })
+  },
+  bindTextAreaBlur: function(e) {
+    console.log(e.detail.value)
+  },
+  bindFormSubmit: function(e) {
+    console.log(e.detail.value.textarea)
+  },
+  audioPlay: function() {
+    this.audioCtx.play()
+  },
+  audioPause: function() {
+    this.audioCtx.pause()
+  },
+  audio14: function() {
+    this.audioCtx.seek(14)
+  },
+  audioStart: function() {
+    this.audioCtx.seek(0)
+  },
+
+  bindInputBlur: function(e) {
+    this.inputValue = e.detail.value
+  },
+  bindButtonTap: function() {
+    var that = this
+    wx.chooseVideo({
+      sourceType: ['album', 'camera'],
+      maxDuration: 60,
+      camera: ['front', 'back'],
+      success: function(res) {
+        that.setData({
+          src: res.tempFilePath
+        })
+      }
+    })
+  },
+  bindSendDanmu: function() {
+    this.videoContext.sendDanmu({
+      text: this.inputValue,
+      color: getRandomColor()
+    })
+  },
+  takePhoto() {
+    const ctx = wx.createCameraContext()
+    ctx.takePhoto({
+      quality: 'high',
+      success: (res) => {
+        this.setData({
+          src: res.tempImagePath
+        })
+      }
+    })
+  },
+  error(e) {
+    console.log(e.detail)
+  }
+
 })
